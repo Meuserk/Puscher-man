a3d443847c102804a93503d9cfad3cc1274586db
Meuserk <50529664+Meuserk@users.noreply.github.com>
2024-11-18 13:05:40 +0100

    Update Paket.java
    add a construktor for scaling of the image

--- Paket.java
+++ Paket.java
@@ -8,6 +8,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Paket extends Actor
 {
+    public Paket(int x, int y){
+        GreenfootImage image = getImage();   // Holt das aktuelle Bild des Objekts
+        image.scale(x, y);               // Skalieren des Bildes auf 100x100 Pixel
+        setImage(image);                     // Setzt das skalierte Bild zurück auf das Objekt
+    }
     /**
      * Act - do whatever the Paket wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
